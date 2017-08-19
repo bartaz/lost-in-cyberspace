fd14cb22434047c8bf02ba29283f129187307ddf
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-08-19 18:49:49 +0200

    Example network rendering

--- network/network.js
+++ network/network.js
@@ -29,28 +29,39 @@ function randomInt(max) {
 }
 
 function randomWalls() {
-  return {
-    xSpread: randomInt(6) + 1,
-    xOffset: randomInt(7),
-    ySpread: randomInt(6) + 1,
-    yOffset: randomInt(7)
-  }
-}
+  let xSpread = randomInt(6) + 1;
+  let xOffset = randomInt(7);
+  let xWalls = computeWalls(xSpread, xOffset);
 
-function getWalls(params) {
+  let ySpread = randomInt(6) + 1;
+  let yOffset = randomInt(7);
+  let yWalls = computeWalls(ySpread, yOffset);
+
+  // prefent non connected nodes from appearing in the corners
+  if (
+    (xWalls[0] === 1 && yWalls[0] === 1) ||
+    (xWalls[7] === 7 && yWalls[7] === 7) ||
+    (xWalls[0] === 7 && yWalls[7] === 1) ||
+    (xWalls[7] === 1 && yWalls[0] === 7)
+  ) {
+    ySpread = randomInt(6) + 1;
+    yOffset = randomInt(7);
+    yWalls = computeWalls(ySpread, yOffset);
+  }
+
   return {
-    xSpread: params.xSpread,
-    xOffset: params.xOffset,
-    ySpread: params.ySpread,
-    yOffset: params.yOffset,
-    xWalls: computeWalls(params.xSpread, params.xOffset),
-    yWalls: computeWalls(params.ySpread, params.yOffset)
+    xSpread: xSpread,
+    xOffset: xOffset,
+    ySpread: ySpread,
+    yOffset: yOffset,
+    xWalls: xWalls,
+    yWalls: yWalls
   }
 }
 
 function randomNetwork() {
   return {
-    walls: getWalls(randomWalls())
+    walls: randomWalls()
   }
 }
 
@@ -80,3 +91,53 @@ function runTests() {
 
   console.groupEnd();
 }
+
+function getNetworkMap(network) {
+  // 0 - wall (nothing)
+  // 1 - connection h
+  // 2 - connection v
+  // 3 - node
+  //const nodesLine = '&block;&boxh;&block;&boxh;&block;&boxh;&block;&boxh;&block;&boxh;&block;&boxh;&block;&boxh;&block;';
+  //const linksLine = '&boxv; &boxv; &boxv; &boxv; &boxv; &boxv; &boxv; &boxv;';
+  const nodesLine = [3,1,3,1,3,1,3,1,3,1,3,1,3,1,3];
+  const linksLine = [2,0,2,0,2,0,2,0,2,0,2,0,2,0,2];
+  let networkGrid = []
+
+  for (let i = 0; i < 8; i++) {
+    networkGrid.push(nodesLine.slice(0));
+    if (i < 7) {
+      networkGrid.push(linksLine.slice(0));
+    }
+  }
+
+  if (network && network.walls) {
+    let walls;
+    if (network.walls.xWalls) {
+      walls = network.walls.xWalls;
+      for (i = 0; i < 8; i++) {
+        if (walls[i]) {
+          networkGrid[i * 2][walls[i] * 2 - 1] = 0;
+        }
+      }
+    }
+
+    if (network.walls.yWalls) {
+      walls = network.walls.yWalls;
+      for (i = 0; i < 8; i++) {
+        if (walls[i]) {
+          networkGrid[walls[i] * 2 - 1][i * 2] = 0;
+        }
+      }
+    }
+  }
+  // turn magic numbers into HTML friendly string representation
+  let networkString = networkGrid.map(function(line){
+    return line.join('').replace(/0/g, ' ').replace(/1/g, '&boxh;').replace(/2/g, '&boxv;').replace(/3/g, '@');
+  }).join('\n');
+
+  return networkString;
+}
+
+var n = randomNetwork();
+console.log(n);
+document.body.innerHTML = getNetworkMap(n);
